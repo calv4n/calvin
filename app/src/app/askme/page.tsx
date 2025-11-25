4a04d66e48002289e4a9a5d7b29b15b4a79c67d0
calv4n <calvin.pfrender@gmail.com>
2025-11-25 15:41:30 +0100

    background color changed askme site

--- app/src/app/askme/page.tsx
+++ app/src/app/askme/page.tsx
@@ -4,7 +4,7 @@ import AnimatedContent from "@/components/AnimatedContent";
 
 export default function Askme() {
     return (
-        <section id="projects" className="p-[48px] bg-[#1c1c1c] text-white pt-24">
+        <section id="projects" className="p-[48px] bg-[#fff9f0]">
             <div className="max-w-7xl mx-auto">
                 <AnimatedContent
                     distance={70}
