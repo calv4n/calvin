ca4aed4e3c9e70c1cabacd71eb64dd72fe1e8310
calv4n <85294058+calv4n@users.noreply.github.com>
2025-11-26 22:03:02 +0100

    answerpanel edited

--- app/src/app/askme/components/AnswerPanel.tsx
+++ app/src/app/askme/components/AnswerPanel.tsx
@@ -9,10 +9,9 @@ export default function AnswerPanel({ answer, error }: AnswerPanelProps) {
     if (!answer && !error) return null;
 
     return (
-        <>
+        <div className="max-w-[450px] md:w-1/2 flex flex-col">
             {answer && (
-                <div className="mb-4 text-sm text-gray-800">
-                    <p className="font-semibold mb-1">Answer:</p>
+                <div className="mb-4 text-sm text-gray-800 bg-white/80 p-3 pr-4 pl-4 rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl rounded-br-md shadow-[0_12px_40px_rgba(0,0,0,0.08)] ">
                     <p className="leading-relaxed whitespace-pre-line">{answer}</p>
                 </div>
             )}
@@ -22,6 +21,6 @@ export default function AnswerPanel({ answer, error }: AnswerPanelProps) {
                     <p className="leading-relaxed">{error}</p>
                 </div>
             )}
-        </>
+        </div>
     );
 }
