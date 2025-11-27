9735d6f4702b24bf520f2af6f5b2195ceaf122c2
calv4n <calvin.pfrender@gmail.com>
2025-11-27 08:53:24 +0100

    added border to answer panel

--- app/src/app/askme/components/AnswerPanel.tsx
+++ app/src/app/askme/components/AnswerPanel.tsx
@@ -11,7 +11,7 @@ export default function AnswerPanel({ answer, error }: AnswerPanelProps) {
     return (
         <div className="max-w-[450px] md:w-1/2 flex flex-col">
             {answer && (
-                <div className="mb-4 text-sm text-gray-800 bg-white/80 p-3 pr-4 pl-4 rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl rounded-br-md shadow-[0_12px_40px_rgba(0,0,0,0.08)] ">
+                <div className="mb-4 text-sm text-gray-800 bg-white/80 p-3 pr-4 pl-4 rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl rounded-br-md border shadow-[0_12px_40px_rgba(0,0,0,0.08)] ">
                     <p className="leading-relaxed whitespace-pre-line">{answer}</p>
                 </div>
             )}
