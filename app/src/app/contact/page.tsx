b081258fe56f40f2bf2e580642da3eace955402a
calv4n <calvin.pfrender@gmail.com>
2025-11-28 11:29:10 +0100

    package json dev host edited to view local on phone. finished background transition on contact

--- app/src/app/contact/page.tsx
+++ app/src/app/contact/page.tsx
@@ -7,8 +7,10 @@ export default function Projects() {
             id="contact"
             className="relative overflow-hidden bg-[#fff9f0] text-black px-[48px] pt-40 md:pt-48"
         >
-            <div className="pointer-events-none select-none absolute -top-36 -left-[6%] w-[112%] h-64 md:h-72 bg-[#1c1c1c] skew-y-5 origin-top-left" />
-            <div className="pointer-events-none select-none absolute top-10 right-[10%] w-[36%] h-60 bg-[#1c1c1c] skew-y-2 rotate-1 origin-top-right md:top-8 md:h-68" />
+            <div className="pointer-events-none select-none absolute inset-0 overflow-hidden">
+                <div className="absolute -top-28 -left-[10%] w-[125%] h-72 md:h-80 bg-[#1c1c1c] rotate-[3deg] rounded-[38px] shadow-2xl shadow-[#1c1c1c]/25 transform-gpu" />
+                <div className="absolute -top-4 right-[-8%] w-[32%] md:w-[34%] h-60 md:h-95 bg-gradient-to-b bg-[#1c1c1c] rotate-[3deg] md:rotate-[3deg] shadow-2xl shadow-[#1c1c1c]/25 transform-gpu" />
+            </div>
 
             <div className="relative max-w-7xl mx-auto">
                 <AnimatedContent
@@ -26,12 +28,12 @@ export default function Projects() {
                     <div className="min-h-[calc(70vh-96px)] pt-8 md:pt-12">
                         <div>
                             <div>
-                                <h1 className="text-8xl font-bold m-5">LET'S BUILD</h1>
+                                <h1 className="text-5xl md:text-8xl font-bold m-5">LET'S BUILD</h1>
                             </div>
                             <div className="flex flex-row">
-                                <h1 className="text-8xl font-bold m-5">THE</h1>
+                                <h1 className="text-5xl md:text-8xl font-bold m-5">THE</h1>
                                 <Highlighter action="highlight" color="#ffc700" animationDuration={1600} isView={true}>
-                                    <h1 className="text-8xl font-bold m-5">FUTURE</h1>
+                                    <h1 className="text-5xl md:text-8xl font-bold m-5">FUTURE</h1>
                                 </Highlighter>
                             </div>
                         </div>
