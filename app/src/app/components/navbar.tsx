5322a71821097c806fbaafe53cb5ad139e5e7c83
calv4n <85294058+calv4n@users.noreply.github.com>
2025-11-30 23:56:48 +0100

    minor changes

--- app/src/app/components/navbar.tsx
+++ app/src/app/components/navbar.tsx
@@ -126,7 +126,7 @@ export default function Navbar() {
                     <Link
                       href={href}
                       onClick={() => setIsOpen(false)}
-                      className="flex w-full items-center justify-between rounded-lg px-3 py-2 transition hover:bg-black/5"
+                      className="flex w-full items-center justify-between rounded-lg px-1 py-2 transition hover:bg-black/5"
                     >
                       <span>{name}</span>
                     </Link>
@@ -135,7 +135,7 @@ export default function Navbar() {
                   <Link
                     href={href}
                     onClick={() => setIsOpen(false)}
-                    className="flex w-full items-center justify-between rounded-lg px-3 py-2 transition hover:bg-black/5"
+                    className="flex w-full items-center justify-between rounded-lg px-1 py-2 transition hover:bg-black/5"
                   >
                     <span>{name}</span>
                   </Link>
