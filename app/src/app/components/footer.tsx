dcbdf83fef60af5457e2f30ecb5b363cb60350b3
calv4n <85294058+calv4n@users.noreply.github.com>
2025-12-01 00:28:20 +0100

    contact link on footer

--- app/src/app/components/footer.tsx
+++ app/src/app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full h-16 flex justify-around mt-auto bg-[#fff9f0]">
       <p className="uppercase text-sm text-gray-700 opacity-50">© 2025 Calvin Pfrender</p>
-      <p className="uppercase text-sm text-gray-700 opacity-50">contact</p>
+      <a href="mailto:calvin.pfrender@gmail.com" className="uppercase text-sm text-gray-700 opacity-50">contact</a>
     </footer>
   );
 }
